3a52e08755dbf6a03e42ec81d9477aaaf6baa7df
pfxuchn <pfxuchn@gmail.com>
2024-10-04 18:12:38 +0800

    [CI] 增加编译镜像

---
+++ start-build-env.sh
@@ -0,0 +1,15 @@
+user=$(whoami)
+
+docker build -t img-build-ubuntu:"${user}" -f- . <<EOF
+FROM ubuntu:20.04
+RUN apt-get update -y && apt-get install curl -y && curl https://sh.rustup.rs -sSf | sh -s -- -y
+EOF
+
+
+docker run --rm -v "${PWD}":/workspace -w /workspace img-build-ubuntu:"${user}" /bin/sh -c "
+    rustup default stable &&
+    cargo build --release &&
+    chmod 777 target/*
+"
+
+
