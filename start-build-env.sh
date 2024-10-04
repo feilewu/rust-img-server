7f2b5c3ea4329c996b3c6ea368a954a7e1353fd3
pfxuchn <pfxuchn@gmail.com>
2024-10-04 20:45:25 +0800

    [CI] 增加路径打印

--- start-build-env.sh
+++ start-build-env.sh
@@ -7,8 +7,11 @@ FROM ubuntu:20.04
 RUN apt-get update -y && apt-get install curl -y && apt-get install build-essential && curl https://sh.rustup.rs -sSf | sh -s -- -y
 EOF
 
+echo ${script_dir}
 
 docker run --rm -v "${script_dir}":/workspace -w /workspace img-build-ubuntu:"${user}" /bin/sh -c "
+    echo $PWD
+    ls .
     . /root/.cargo/env &&
     rustup default stable &&
     cargo build --release &&
